0b73c76d93a914656d49638a7a6f1e691186d5ac
Codingfor1 <Keira.sullivan28@gmail.com>
2023-03-01 20:57:56 -0500

    boolean

--- NetworkTablePublisher.py
+++ NetworkTablePublisher.py
@@ -16,6 +16,14 @@ def publishString(MergeVisionPipeLineTableName,name, Strvalue):
     networktable.putString(name, Strvalue)
     #print(name+ ": " + str(value))    
 
+def publishBoolean(MergeVisionPipeLineTableName,name, Booleanvalue):
+    #start NetworkTables
+    ntinst = NetworkTableInstance.getDefault()
+    # Name of network table - this is how it communicates with robot. IMPORTANT
+    networktable = ntinst.getTable(MergeVisionPipeLineTableName)
+    networktable.putBoolean(name, Booleanvalue)
+    #print(name+ ": " + str(value))  
+
 def publishNumberArray(MergeVisionPipeLineTableName,name, numarray):
     #start NetworkTables
     ntinst = NetworkTableInstance.getDefault()
