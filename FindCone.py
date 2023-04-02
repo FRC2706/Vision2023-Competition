b1944bb5bfcd0db149e8edcfc79a0add143b83c3
Codingfor1 <Keira.sullivan28@gmail.com>
2023-04-02 14:42:20 -0400

    base/nose

--- FindCone.py
+++ FindCone.py
@@ -54,41 +54,25 @@ def findCone(frame, MergeVisionPipeLineTableName, CameraFOV):
         # Shows the contours overlayed on the original video
 
     #Crop image
-    img = cv2.imread("./2023VisionSampleImages/baseIn.png")
-    cropped_image = img[180:380, 430:560]
-    cv2.imshow("cropped", cropped_image)
-    # Save the cropped image
-    cv2.imwrite("./2023VisionSampleImages/cropped.jpg", cropped_image)
+    cropped_image_one = image[180:380, 430:560]
 
-    img = cv2.imread("./2023VisionSampleImages/baseIn.png")
-    cropped_image = img[180:380, 230:360]
-    cv2.imshow("cropped_two", cropped_image)
-    # Save the cropped image
-    cv2.imwrite("./2023VisionSampleImages/cropped_two.jpg", cropped_image)
+    cropped_image_two = image[180:380, 230:360]
 
-    
     #get average colour value of cropped image
-    src_img = cv2.imread("./2023VisionSampleImages/cropped.jpg")
-    hsv = cv2.cvtColor(src_img, cv2.COLOR_BGR2HSV)
-    average_color_row_one = np.average(src_img, axis=0)
+    hsv = cv2.cvtColor(cropped_image_one, cv2.COLOR_BGR2HSV)
+    average_color_row_one = np.average(hsv, axis=0)
     average_color_one = np.average(average_color_row_one, axis=0)
     print(average_color_one)
-    d_img = np.ones((312,312,3), dtype=np.uint8)
-    d_img[:,:] = average_color_one
-    cv2.imshow('Average Color one',d_img)
 
-    src_img = cv2.imread("./2023VisionSampleImages/cropped_two.jpg")
-    hsv = cv2.cvtColor(src_img, cv2.COLOR_BGR2HSV)
-    average_color_row_two = np.average(src_img, axis=0)
+    hsv = cv2.cvtColor(cropped_image_two, cv2.COLOR_BGR2HSV)
+    average_color_row_two = np.average(hsv, axis=0)
     average_color_two = np.average(average_color_row_two, axis=0)
     print(average_color_two)
-    d_img = np.ones((312,312,3), dtype=np.uint8)
-    d_img[:,:] = average_color_two
-    cv2.imshow('Average Color two',d_img)
 
-
-    cv2.waitKey(0)
-
+    if average_color_one[2] > average_color_two[2]:
+        print ("nose in")
+    else:
+        print("base in")
     return image, Yaw, area
 
 def findCones(cntsSorted, image, CameraFOV):
