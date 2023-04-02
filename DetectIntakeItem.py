65873e6a268e460b17516518892774fda631c4f7
Codingfor1 <Keira.sullivan28@gmail.com>
2023-04-02 16:06:36 -0400

    it just works!

--- DetectIntakeItem.py
+++ DetectIntakeItem.py
@@ -40,8 +40,8 @@ def DetectIntakeItem(frame, MergeVisionPipeLineTableName):
         contours, _ = cv2.findContours(MaskPurple, cv2.RETR_TREE, cv2.CHAIN_APPROX_TC89_KCOS)
     FoundPurple, _ = FindRectFillAmount(contours,H,W, image)
     
-    cv2.putText(frame, "Yellow: " + str(FoundYellow), (10, 350), cv2.FONT_HERSHEY_COMPLEX, .9, white)
-    cv2.putText(frame, "Purple: " + str(FoundPurple), (10, 375), cv2.FONT_HERSHEY_COMPLEX, .9, white)
+    #cv2.putText(frame, "Yellow: " + str(FoundYellow), (10, 350), cv2.FONT_HERSHEY_COMPLEX, .9, white)
+    #cv2.putText(frame, "Purple: " + str(FoundPurple), (10, 375), cv2.FONT_HERSHEY_COMPLEX, .9, white)
     #cv2.putText(image, "Fill Value: " + str(DesiredRectFilledArea), (450, 360), cv2.FONT_HERSHEY_COMPLEX, .4, white)
     # pushes cargo angle to network tables
     #publishNumber(MergeVisionPipeLineTableName, "YawToCargo", finalTarget[0])
@@ -50,12 +50,26 @@ def DetectIntakeItem(frame, MergeVisionPipeLineTableName):
     #cv2.line(image, (round(x), round(y)), (round(x+w), round(y+h)), white, 2)
     #cv2.imshow("area",image)
     
+
+    cropped_image_one = image[round(H/4):round(H/2), round(W/4):round(3*W/4)]
+    hsv = cv2.cvtColor(cropped_image_one, cv2.COLOR_BGR2HSV)
+    average_color_row_one = np.average(hsv, axis=0)
+    average_color_one = np.average(average_color_row_one, axis=0)
+    print(average_color_one)
+
+    cropped_image_two = image[round(3*H/4):round(H-1), round(W/4):round(3*W/4)]
+    hsv = cv2.cvtColor(cropped_image_two, cv2.COLOR_BGR2HSV)
+    average_color_row_two = np.average(hsv, axis=0)
+    average_color_two = np.average(average_color_row_two, axis=0)
+    print(average_color_two)
+
+    if (average_color_one[2]> average_color_two[2]+8):
+        print ("nose in in in in")
+    else:
+        print("base in")
+        
     if FoundYellow:
         publishBoolean(MergeVisionPipeLineTableName, "DetectCone", True)
-        if coneRectFilledArea < 0.85:
-            publishBoolean(MergeVisionPipeLineTableName, "ConeNoseIn", True)
-        else:
-            publishBoolean(MergeVisionPipeLineTableName, "ConeNoseIn", False)
     else:
         publishBoolean(MergeVisionPipeLineTableName, "DetectCone", False)
     if FoundPurple:
